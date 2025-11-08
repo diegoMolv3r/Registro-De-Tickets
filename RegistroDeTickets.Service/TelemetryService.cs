373336bae8a2b66d191f8fb3d95c597f08fe0368
nseehofer <nicolasseehofer.99@hotmail.com>
2025-11-08 13:41:50 -0300

    Se crea servicio de telemetria

---
+++ RegistroDeTickets.Service/TelemetryService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using RegistroDeTickets.Data.Entidades;
+
+namespace RegistroDeTickets.Service
+{
+    public interface ITelemetryService
+    {
+        void RegistrarEvento(string nombreEvento, Usuario? usuario);
+    }
+    public class TelemetryService : ITelemetryService
+    {
+        private readonly TelemetryClient _telemetryClient;
+        public TelemetryService (TelemetryClient telemetryClient)
+        {
+            _telemetryClient = telemetryClient;
+        }
+
+        // Las 'propiedades' son para la informacion adicional que se quiera agregar al evento
+        public void RegistrarEvento(string nombreEvento, Usuario? usuario)
+        {
+            var propiedades = new Dictionary<string, string>();
+            
+            if (usuario != null)
+            {
+                propiedades.Add("UsuarioId", usuario.Id.ToString());
+                propiedades.Add("Email", usuario.Email);
+                propiedades.Add("Username", usuario.Username);
+            }
+            else
+            {
+
+                propiedades.Add("UsuarioId", "No existe");
+                _telemetryClient.TrackEvent(nombreEvento, propiedades);
+            }
+
+            
+        }
+
+       
+
+    }
+}
